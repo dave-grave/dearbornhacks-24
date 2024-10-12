3bd3356f194d89ab7c1e7f63d8864a31f103e7f0
David Huang <djhuang636@gmail.com>
2024-10-12 16:15:34 -0400

    add chat functionality to text_generation.py

--- text_generation.py
+++ text_generation.py
@@ -14,15 +14,32 @@ if api_key is None:
 genai.configure(api_key=os.environ['API_KEY'])
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-# prompts
-default_prompt = "You are a Michigan Engineering student. Weigh the options of your major choices for different career paths."
-user_input = input("Enter prompt: ")
-prompt = user_input if user_input.strip() else default_prompt
-response = model.generate_content(f"""Given a definition, return the word it defines.
-                                        Definition: When you're happy that other people are also sad.
-                                        Word: schadenfreude
-                                        Definition: existing purely in the mind, but not in physical reality
-                                        Word: abstract
-                                        Definition: ${prompt}
-                                        Word:""")
-print(response.text)
+# start chat with basic conversation prompts
+chat = model.start_chat(
+            history=[
+                {"role": "user", "parts": "Hello"},
+                {"role": "model", "parts": "Great to meet you. What would you like to know?"},
+            ]
+        )
+
+# take in input as long as user doesn't reply with 'q'
+while(True):
+    prompt = input('Enter a reply: ')
+    if prompt == 'q':
+        break
+
+    response = chat.send_message(prompt)
+    print(response.text)
+
+# PROMPTS TESTING
+# default_prompt = "You are a Michigan Engineering student. Weigh the options of your major choices for different career paths."
+# user_input = input("Enter prompt: ")
+# prompt = user_input if user_input.strip() else default_prompt
+# response = model.generate_content(f"""Given a definition, return the word it defines.
+#                                         Definition: When you're happy that other people are also sad.
+#                                         Word: schadenfreude
+#                                         Definition: existing purely in the mind, but not in physical reality
+#                                         Word: abstract
+#                                         Definition: ${prompt}
+#                                         Word:""")
+# print(response.text)
